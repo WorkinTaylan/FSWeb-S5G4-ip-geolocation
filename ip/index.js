9ba17e8d3d32c05a74c6d438d6be8897136e6b61
WorkinTaylan <acikgozta@gmail.com>
2023-01-27 13:38:25 +0300

    Proje tamamlandı.

--- ip/index.js
+++ ip/index.js
@@ -1,4 +1,4 @@
-//axios import buraya gelecek
+import axios from 'axios'; //axios import buraya gelecek
 
 var benimIP;
 
@@ -13,6 +13,7 @@ async function ipAdresimiAl(){
 		url: 'https://apis.ergineer.com/ipadresim',
 	})
 	.then(function (response) {
+		console.log(response)
 		return response.data
 	})
 	.then(function (a) {
@@ -25,7 +26,7 @@ async function ipAdresimiAl(){
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
     (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
+    https://apis.ergineer.com/ipgeoapi/88.241.232.43
 	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
@@ -54,10 +55,96 @@ async function ipAdresimiAl(){
     </div>
 */
 
+let dummy={
+	"sorgu":"88.241.232.43",
+	"durum":"OK",
+	"kıta":"Asia",
+	"ülke":"Turkey",
+	"ülkeKodu":"TR",
+	"ülkebayrağı":"https:\/\/apis.ergineer.com\/ulkebayraklari\/TR",
+	"bölge":"06",
+	"bölgeAdı":"Ankara",
+	"şehir":"Ankara",
+	"zip":"06460",
+	"enlem":39.92049999999999698729880037717521190643310546875,
+	"boylam":32.83720000000000283080225926823914051055908203125,
+	"saatdilimi":"Europe\/Istanbul",
+	"parabirimi":"TRY",
+	"isp":"TurkTelecom",
+	"organizasyon":"Turk Telekomunikasyon A.S",
+	"as":"AS47331 TTNet A.S."
+  }
+let cards=document.querySelector(".cards");
+
+function ipInfo(ipObj){
+
+	const cardDiv= document.createElement("div")
+	cardDiv.classList.add("card")
+
+	const img=document.createElement("img")
+	img.src=ipObj.ülkebayrağı
+	cardDiv.appendChild(img)
+
+	const cardInfo=document.createElement("div")
+	cardInfo.classList.add("card-info")
+	cardDiv.appendChild(cardInfo)
+
+	const ip=document.createElement("h3")
+	ip.classList.add("ip")
+	ip.textContent=ipObj.sorgu
+	cardInfo.appendChild(ip)
+
+	const ulke=document.createElement("p")
+	ulke.textContent=`${ipObj.ülke} (${ipObj.ülkeKodu})`
+	ulke.classList.add("ulke")
+	cardInfo.appendChild(ulke)
+
+	const para1=document.createElement("p")
+	para1.textContent=`Enlem: ${ipObj.enlem} Boylam: ${ipObj.boylam}`;
+	cardInfo.appendChild(para1)
+
+	const para2=document.createElement("p")
+	para2.textContent=`Şehir: ${ipObj.şehir}`
+	cardInfo.appendChild(para2)
+
+	const para3=document.createElement("p")
+	para3.textContent=`Saat dilimi: ${ipObj.saatdilimi}`
+	cardInfo.appendChild(para3)
+
+	const para4=document.createElement("p")
+	para4.textContent=`Para birimi: ${ipObj.parabirimi}`
+	cardInfo.appendChild(para4)
+
+	const para5=document.createElement("p")
+	para5.textContent=`ISP: ${ipObj.isp}`
+	cardInfo.appendChild(para5)
+
+	return cardDiv;
+}
+
+
+
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
 */
+const connection = async function(){
+	await ipAdresimiAl();
+	await axios({
+		method: 'get',
+		url: 'https://apis.ergineer.com/ipgeoapi/'+benimIP,
+	})
+	.then(function (response) {
+		console.log(response)
+		return response.data
+	})
+	.then(function (a) {
+		cards.appendChild(ipInfo(a))
+	});
+}
+connection();
+
+
 
 /*
 	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
